d2c23984ec465f334cb5837fecc4bd3c0a515e38
Avinash Singh Munda <96285074+avimunda777@users.noreply.github.com>
2025-06-12 02:05:20 +0530

    Add files via upload

---
+++ CarObjectDemo.java
@@ -0,0 +1,19 @@
+// Define a class named Car
+class Car {
+    // Method of the Car class
+    void display() {
+        System.out.println("This is a car object.");
+    }
+}
+
+// Main class
+public class CarObjectDemo {
+    public static void main(String[] args) {
+        // Create an object of Car
+        Car myCar = new Car();
+
+        // Call the method using the object
+        myCar.display();
+    }
+}
+
